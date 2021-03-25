ada7907c735bb68ab5c565b0b1ebf3615fbf2574
Ialtice <48520799+Ialtice@users.noreply.github.com>
2021-03-25 16:40:54 +0100

    Add files via upload

---
+++ CarClassActivity/CarClassActivity/CarClassActivity/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CarClassActivity
+{
+    class Car
+    {
+        private int year_model;
+        private string make;
+        private int speed;
+
+        public Car(int inYear_model, string inMake)
+        {
+            year_model = inYear_model;
+            make = inMake;
+            speed = 0;
+        }
+        public void accelerate()
+        {
+            speed += 5;
+        }
+        public void brake()
+        {
+            speed -= 5;
+        }
+        public int current_speed()
+        {
+            return speed;
+        }
+    }
+}
---
+++ CarClassActivity/CarClassActivity/CarClassActivity/Program.cs
@@ -0,0 +1,56 @@
+﻿//Student: Isaac Altice
+// ID # : 801077284
+//Car Activity
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CarClassActivity
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int inYear_Model = 2020; ;
+            string inMake = "Nissan";
+            Console.WriteLine("Enter the Car's Year Model: ");
+            try
+            {
+               inYear_Model = Int32.Parse(Console.ReadLine());
+            }
+            catch(InvalidCastException e)
+            {
+                Console.WriteLine("{0} Exception.", e);
+                Console.WriteLine("Invalid Model year entered, using default of 2020");
+            }
+            Console.WriteLine("Enter the Car's Make: ");
+            try
+            {
+                inMake = Console.ReadLine();
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine("{0} Exception.", e);
+                Console.WriteLine("Invalid make entered, using default of Nissan");
+            }
+            Car car1 = new Car(inYear_Model, inMake);
+            Console.WriteLine("Accelerating.");
+            for(int i = 0; i < 5; i++)
+            {
+                car1.accelerate();
+                Console.WriteLine("Current Speed : " + car1.current_speed());
+            }
+            Console.WriteLine("Braking.");
+            for (int i = 0; i < 5; i++)
+            {
+                car1.brake();
+                Console.WriteLine("Current Speed : " + car1.current_speed());
+            }
+            Console.ReadLine();
+        }
+    }
+}
